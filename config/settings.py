2422d3cb3654f4c1549e5474d76dec51f86e57aa
Alice Nocquet <al.nocquet@free.fr>
2026-05-07 16:41:34 +0200

    chore(deps): remove unused django-filter dependency

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ DEBUG = os.getenv("DEBUG") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    "django_filters",
     "core",
     "projects",
 ]
