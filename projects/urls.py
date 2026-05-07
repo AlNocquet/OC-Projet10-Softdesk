a6c5199fc6d3c9ad5a7e4acbb28338df709ba46f
Alice Nocquet <al.nocquet@free.fr>
2026-05-07 16:17:21 +0200

    fix(comments): add retrieve comment endpoint

--- projects/urls.py
+++ projects/urls.py
@@ -21,6 +21,7 @@ contributor_detail = ContributorViewSet.as_view({"delete": "destroy"})
 
 comment_list = CommentViewSet.as_view({"get": "list", "post": "create"})
 comment_detail = CommentViewSet.as_view({
+    "get": "retrieve",
     "patch": "partial_update",
     "delete": "destroy"
 })
